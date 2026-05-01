d9fd4555762e9635c2a9ed8a00d732a07a74df9e
Eleene Yang <78632503+EleeneY@users.noreply.github.com>
2026-05-01 18:49:12 -0400

    Delete src directory

--- src/main/java/com/example/treasurebox/data/class/TreasureBox.kt
+++
@@ -1,9 +0,0 @@
-package com.example.treasurebox.data.`class`
-
-data class TreasureBox(
-    val id: String,
-    val name: String,
-    val currentBalance: Double,
-    val coverImageUrl: String,
-    val members: List<User>
-)
